1a194bfd2b4108cf0fb7ea463831822586cc9788
elevely <vgg47@mail.ru>
2019-12-12 14:58:40 +0300

    add tips

--- data_generator.py
+++ data_generator.py
@@ -5,8 +5,11 @@ import numpy as np
 import scipy.stats as sts 
 import sys
 
-dataset_size = int(input())
-feature_number = int(input())
+dataset_size = int(input('Введи количество строк в генерируемом датасете'))
+feature_number = int(input('Введи количество фичей в генерируемом датасете'))
+dataset_name = input('Введи название для файла, хранящего датасет, например data.csv')
+labels_name = input('Введи название для файла, хранящего целевую переменную, например labels.csv')
+print('Созданные файлы находятся в директории ./data')
 
 def f(x):
     return x @ sts.uniform.rvs(size=feature_number)
@@ -15,5 +18,5 @@ X = np.random.rand(dataset_size, feature_number)
 y = f(X) + sts.norm(0, scale=0.1).rvs(size=dataset_size)
 
 # save to csv file
-savetxt('./data/data.csv', X, delimiter=',')
-savetxt('./data/label.csv', y, delimiter=',')
+savetxt('./data/' + dataset_name, X, delimiter=',')
+savetxt('./data/' + labels_name, y, delimiter=',')
