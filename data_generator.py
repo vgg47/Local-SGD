63aa91ddcd5e5f5a36e5b8805d6a964521fae229
korvin001 <geller.m@phystech.edu>
2019-12-16 01:49:30 +0300

    API for testing methods

--- data_generator.py
+++ data_generator.py
@@ -6,23 +6,29 @@ import scipy.stats as sts
 import sys
 import os
 
-dataset_size = int(input('Введи количество строк в генерируемом датасете:\n'))
-feature_number = int(input('Введи количество фичей в генерируемом датасете:\n'))
-dataset_name = input('Введи название для файла, хранящего датасет, например data.csv:\n')
-labels_name = input('Введи название для файла, хранящего целевую переменную, например labels.csv:\n')
-print('Созданные файлы находятся в директории ./data')
-
-
 def f(x):
     return x @ sts.uniform.rvs(size=feature_number)
 
+def create_data(dataset_size, feature_number):
+    X = sts.uniform.rvs(loc=1, scale=1, size=(dataset_size, feature_number))
+    y = f(X) + sts.norm(0, scale=0.1).rvs(size=dataset_size)
+    return X, y
 
-X = sts.uniform.rvs(loc=1, scale=1, size=(dataset_size, feature_number))
-y = f(X) + sts.norm(0, scale=0.1).rvs(size=dataset_size)
+def main():
+    dataset_size = int(input('Введи количество строк в генерируемом датасете:\n'))
+    feature_number = int(input('Введи количество фичей в генерируемом датасете:\n'))
+    dataset_name = input('Введи название для файла, хранящего датасет, например data.csv:\n')
+    labels_name = input('Введи название для файла, хранящего целевую переменную, например labels.csv:\n')
+    print('Созданные файлы находятся в директории ./data')
 
-if not os.path.exists('./data'):
-    os.makedirs('./data')
+    X, y = create_data(dataset_size, feature_number)
 
-# save to csv file
-savetxt('./data/' + dataset_name, X, delimiter=',')
-savetxt('./data/' + labels_name, y, delimiter=',')
+    if not os.path.exists('./data'):
+        os.makedirs('./data')
+
+    # save to csv file
+    savetxt('./data/' + dataset_name, X, delimiter=',')
+    savetxt('./data/' + labels_name, y, delimiter=',')
+
+if __name__ == "__main__":
+    main()
---
+++ test.py
@@ -0,0 +1,63 @@
+import argparse
+import functools
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+
+from data_generator import create_data
+from gradient_computing import mse_metric
+from numpy import loadtxt
+from scipy.optimize import minimize
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', '-d',
+                        help='Путь к файлу с dataset', default='./data/default_data.csv')
+    parser.add_argument('--label', '-l',
+                        help='Путь к файлу с label', default='./data/default_labels.csv')
+    return parser.parse_args()
+
+def mse_test(w, X=None, y=None):
+    return mse_metric(X, y, w)
+
+def experiment(mse_test, X, y, method, init=None, sample_size=5):
+    if init is None:
+        init = np.ones(X.shape[1])
+    size = 10
+    k = 1
+    n = X.shape[0]
+    times = []
+    sizes = []
+    while(size <= n):
+        print(size)
+        mse = functools.partial(mse_test, X=X[:size], y=y[:size])
+        sample = []
+        for _ in range(sample_size):
+            start = time.time()
+            minimize(mse, init, method=method)
+            stop = time.time()
+            sample.append(stop-start)
+        times.append(np.array(sample).mean())
+        sizes.append(k)
+        size *= 10
+        k += 1
+
+    return np.array(sizes), np.array(times)
+
+args = parse_args()
+X = loadtxt(args.dataset, delimiter=',')
+y = loadtxt(args.label, delimiter=',')  
+mse = functools.partial(mse_test, X=X, y=y)
+x_plot, y_plot = experiment(mse_test, X, y, 'nelder-mead')
+plt.plot(x_plot, y_plot, label='nelder-mead')
+plt.xticks(np.arange(5)+1, np.array(['$10^1$', '$10^2$', '$10^3$', '$10^4$', '$10^5$']))
+plt.ylabel('Time')
+plt.title('Зависимость времени работы методов от размера датасета')
+plt.legend()
+plt.savefig('./img/test')
+#ans1 = minimize(mse, init)
+#ans2 = minimize(mse, init, method='nelder-mead')
+#ans3 = minimize(mse, init, method='powell')
+#print(ans1.fun, ans2.fun, ans3.fun)
+
+
